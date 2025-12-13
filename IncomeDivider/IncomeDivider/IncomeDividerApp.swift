3d792c747c6aa1cc040331bb82d102ca73b772b3
Arsenty Halimovsky <ghost12@yandex.ru>
2025-12-13 17:49:02 +0300

    Create initial project

---
+++ IncomeDivider/IncomeDivider/IncomeDividerApp.swift
@@ -0,0 +1,17 @@
+//
+//  IncomeDividerApp.swift
+//  IncomeDivider
+//
+//  Created by Арсентий Халимовский on 13.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct IncomeDividerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
