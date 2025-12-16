efbe84f2a1ca7d67236fe260c4a86569de2ea6e0
Arsenty Halimovsky <ghost12@yandex.ru>
2025-12-16 15:31:23 +0300

    Create initial View

--- IncomeDivider/IncomeDivider/ContentView.swift
+++ IncomeDivider/IncomeDivider/ContentView.swift
@@ -2,14 +2,41 @@ import SwiftUI
 
 struct ContentView: View {
 	
+	@State var incomeValue = ""
+	@State var categoryName = ""
+	@State var categoryValue = ""
+	
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        
+		List {
+			
+			Section(header: Text("Income")) {
+				TextField("Income", text: $incomeValue)
+			}
+			
+			Section(header: Text("Categories")) {
+				
+				// this block should be implemented as a single view to add
+				HStack {
+					TextField("Name", text: $categoryName)
+					TextField("Value in %", text: $categoryValue)
+					Text("Total: ")
+				}
+				Button("Add Category") {
+					print("added category")
+				}
+			}
+		}
+		.background(.white)
+
+		Button("Divide") {
+			print("divided")
+		}
+		.frame(width: 200, height: 50)
+		.background(.gray)
+		.foregroundStyle(.white)
+		.clipShape(.capsule)
+		
     }
 }
 
