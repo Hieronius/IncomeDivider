c2114272569b7095c0cda2eb291ea2fe481802c5
Arsenty Halimovsky <ghost12@yandex.ru>
2025-12-17 06:21:02 +0300

    Update ContentView

--- IncomeDivider/IncomeDivider/ContentView.swift
+++ IncomeDivider/IncomeDivider/ContentView.swift
@@ -1,36 +1,57 @@
 import SwiftUI
 
+struct Category: Identifiable {
+	
+	var id = UUID()
+	var name = ""
+	var value = ""
+	var total = ""
+}
+
 struct ContentView: View {
 	
-	@State var incomeValue = ""
-	@State var categoryName = ""
-	@State var categoryValue = ""
+	@State var income = ""
+	@State var categories = [Category()]
+	@State var percentsLeft = ""
+	@State var moneyLeft = ""
+	@State var didCalculateResult = false
 	
     var body: some View {
         
 		List {
 			
 			Section(header: Text("Income")) {
-				TextField("Income", text: $incomeValue)
+				TextField("Income", text: $income)
 			}
 			
 			Section(header: Text("Categories")) {
 				
-				// this block should be implemented as a single view to add
-				HStack {
-					TextField("Name", text: $categoryName)
-					TextField("Value in %", text: $categoryValue)
-					Text("Total: ")
+				ForEach($categories) { $category in
+					
+					// this block should be implemented as a single view to add
+					HStack {
+						TextField("Name", text: $category.name)
+						TextField("Value in %", text: $category.value)
+						Text("Total: \(category.total)")
+					}
 				}
 				Button("Add Category") {
-					print("added category")
+					categories.append(Category())
+				}
+			}
+			
+			if didCalculateResult {
+				Section(header: Text("Results")) {
+					Text("Money left: \(moneyLeft)")
+					Text("% of summ left: \(percentsLeft)")
 				}
 			}
 		}
 		.background(.white)
 
 		Button("Divide") {
-			print("divided")
+			print("HELLO FUCKER???")
+			divideIncome()
 		}
 		.frame(width: 200, height: 50)
 		.background(.gray)
@@ -38,6 +59,36 @@ struct ContentView: View {
 		.clipShape(.capsule)
 		
     }
+}
+
+// MARK: Logic
+
+extension ContentView {
+	
+	func divideIncome() {
+		
+		var totalMoney = Double(income) ?? 0
+		var totalPercentage = 100
+		
+		let onePercentIncome = Int(totalMoney / 100)
+		for index in categories.indices {
+			let category = categories[index]
+			let total = (Double(category.value) ?? 0) * Double(onePercentIncome)
+			categories[index].total = String(total)
+			
+			totalMoney -= total
+			totalPercentage -= Int(category.value) ?? 0
+		}
+		moneyLeft = String(totalMoney)
+		percentsLeft = String(totalPercentage)
+		DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+			self.didCalculateResult = true
+		}
+	}
+	
+	func addCategory() {
+		categories.append(Category())
+	}
 }
 
 #Preview {
